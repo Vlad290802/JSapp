e74b4d2b58a0247b0e5b8c21dae4db926fa60c71
Vlad <dvlad4503@gmail.com>
2020-07-06 10:47:38 +0300

    practic 1

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict"
+//1
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
+
+//2
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+//3
+const a = prompt("Один из роследних просмотренных фильмов?", " "),
+      b = +prompt("На сколько оцените его?", " "),
+      c = prompt("Один из роследних просмотренных фильмов?", " "),
+      d = +prompt("На сколько оцените его?", " ");
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
